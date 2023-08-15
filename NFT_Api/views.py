05aca19b7a9e7eacd248b06d7afea586e3da9b22
Maruf512 <marufsarkar512@gmail.com>
2023-08-15 08:43:56 +0600

    done syntex

---
+++ NFT_Api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+anime_list = [
+    {'id': 1, 'name': "One Piece"},
+    {'id': 2, 'name': "Your Name"},
+    {'id': 3, 'name': "Garden of words"},
+    {'id': 4, 'name': "Violate Evergarden"},
+    {'id': 5, 'name': "To The Moon for You"},
+    {'id': 6, 'name': "One Punch Man"},
+]
+
+
+# Create your views here.
+def home(request):
+    context = {'anime_list': anime_list}
+    return render(request, 'home.html', context)
+
+def room(request):
+    return render(request, 'room.html')
